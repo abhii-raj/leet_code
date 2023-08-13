24d93a7da804139e4fd0d8c61339aac95b85af6d
Abhishek Raj <122792587+abhii-raj@users.noreply.github.com>
2023-08-13 10:38:56 +0530

    Time: 523 ms (99.01%), Space: 29.2 MB (83.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,16 +3,28 @@ class Solution:
         start = 0 
         end = len(height)-1
         largest = 0
+        prev_start = 0
+        prev_end = 0
 
         while start != end:
-            next_area = min(height[start], height[end]) * (end - start)
+
+            if height[start] < prev_start:
+                start +=1
+                continue
+            if height[end] < prev_end:
+                end -= 1
+                continue
+
+            next_area = (min(height[start], height[end])) * (end - start)
 
             if next_area > largest:
                 largest = next_area
 
             if height[start] < height[end]:
+                prev_start = height[start]
                 start+=1
             else:
+                prev_end = height[end]
                 end -=1
-            
+
         return largest
